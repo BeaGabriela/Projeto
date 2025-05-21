0502af0b53f7df539ee51916431fe0d51e9914cd
BeaGabriela <biiagabrielaa@gmail.com>
2025-05-21 15:41:18 -0300

    Arquivo onde são agregados todos os mecanismos e dependências a serem usadas, além de definir a porta e  o arquivo 'routes'.

---
+++ BACKEND_NODE/index.js
@@ -0,0 +1,25 @@
+//Requeirindo o dotenv e configurando ele
+require('dotenv').config()
+//Criando uma variavel chamada PORT e atribuindo a ela o.env e o respectivo arquivo ou, caso haja problema com o docuemnto, a porta escolhida sera a 5000
+const PORT = process.env.PORT || 5000
+//Criando uma variavel para hospedar o express
+const express = require("express")
+//Criando uma bariavel para armazenar o mecanismo de segurança conhecido como cors.
+const cors = require('cors')
+
+//Criando uma variavel que armazenara todo o meu arquivo clientes.routes, e com ele eu posso acessar as funções
+const clientes = require('./src/Routes/Clientes.routes.js')
+
+//Criando a chamada de todos os mecanismos e dependencias a serem usada, inclusive a variavel setada para acessar o clientes.routes
+const app = express()
+    .use(express.json())
+    .use(cors())
+    .use(clientes)
+
+
+//Chamando o app e atribuindo a ele a porta a ser executada,e  tambem um console log, que mostrara no console em qual porta o serviço está sendo executado.
+app.listen(PORT, () => {
+    console.log("Serviço em execução na porta " + PORT )
+})
+
+
