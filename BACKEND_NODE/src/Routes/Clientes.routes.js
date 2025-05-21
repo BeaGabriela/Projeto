58acc7982dc4155b2a28c411190ade41dbb8f901
BeaGabriela <biiagabrielaa@gmail.com>
2025-05-21 15:26:40 -0300

    Agregação de imagens a cada pizza cadastrada. Atráves do caminho relativo.

---
+++ BACKEND_NODE/src/Routes/Clientes.routes.js
@@ -0,0 +1,19 @@
+//Defindo uma variavel e atribuindo o express a ela
+const express = require('express')
+//Defininfo uma variavel que atraves do express, chama o ROuter
+const router = express.Router()
+
+//Criando uma variaval que herda as funções do arquivo todo
+const clientes = require('../Controllers/Clientes.controller.js')
+
+//Definindo o corpo da URL, para puxr todos os clientes.
+router.get("/clientes", clientes.lerTodos)
+//Definindo o corpo da URL para logar
+router.post('/logar', clientes.Logar)
+//Definindo o corpo da URL para puxar clientes filtrados pelo id
+router.get("/clientes/:id_cliente", clientes.lerClientesId)
+//Definindo o corpo da URL para criar um cliente novo
+router.post("/clientes", clientes.criandoNovoCliente)
+
+//Exportando o router
+module.exports = router;
