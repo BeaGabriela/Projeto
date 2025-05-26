e8ba3a25fb2ae176f07bd55f8ffe9e8226ee500f
BeaGabriela <biiagabrielaa@gmail.com>
2025-05-26 14:55:41 -0300

    Adicionando o token na rota de login.

--- BACKEND_NODE/src/Routes/Clientes.routes.js
+++ BACKEND_NODE/src/Routes/Clientes.routes.js
@@ -6,10 +6,13 @@ const router = express.Router()
 //Criando uma variaval que herda as funções do arquivo todo
 const clientes = require('../Controllers/Clientes.controller.js')
 
+//Criando uma variavel que herda a funções do arquivo middleware
+const Middle = require('../Middleware/Middleware');
+
 //Definindo o corpo da URL, para puxr todos os clientes.
 router.get("/clientes", clientes.lerTodos)
 //Definindo o corpo da URL para logar
-router.post('/logar', clientes.Logar)
+router.post('/logar', Middle.Autenticar, clientes.Logar)
 //Definindo o corpo da URL para puxar clientes filtrados pelo id
 // router.get("/clientes/:id_cliente", clientes.lerClientesId)
 //Definindo o corpo da URL para criar um cliente novo
