b2cfde75c17e573270eebaebeac8f39ade3f7c69
BeaGabriela <biiagabrielaa@gmail.com>
2025-05-27 14:54:26 -0300

    Criando as rotas que atendem ao controllers e adicionando o token em rotas especificas como a de alteração de dados do usuario e a de deletar conta.

--- BACKEND_NODE/src/Routes/Clientes.routes.js
+++ BACKEND_NODE/src/Routes/Clientes.routes.js
@@ -10,13 +10,25 @@ const clientes = require('../Controllers/Clientes.controller.js')
 const Middle = require('../Middleware/Middleware');
 
 //Definindo o corpo da URL, para puxr todos os clientes.
-router.get("/clientes", clientes.lerTodos)
-//Definindo o corpo da URL para logar
-router.post('/logar', Middle.Autenticar, clientes.Logar)
+router.get("/clientes", clientes.LerClientes)
+
 //Definindo o corpo da URL para puxar clientes filtrados pelo id
-// router.get("/clientes/:id_cliente", clientes.lerClientesId)
+router.get("/clientes/:id_cliente", clientes.LerClientesFiltradoId)
+
+//Definindo o corpo da URL para que o usuario possa logar
+router.post('/logar', clientes.Logar)
+
+//Definindo o corpo da url para que o usuario possa ler suas informações
+router.get("/usuario/informacoes", Middle.Autenticar, clientes.LerInformacoesPessoais)
+
 //Definindo o corpo da URL para criar um cliente novo
-router.post("/clientes", clientes.criandoNovoCliente)
+router.post("/clientes", clientes.CriarCliente)
+
+//Definindo o corpo da URL para alterar dados pessoais
+router.put("/usuario/alterar", Middle.Autenticar,  clientes.AlterarDadosPessoais)
+
+//Definindo o corpo da URL para deletar cadastro
+router.delete("/usuario/deletar", Middle.Autenticar,  clientes.DeletarCadastro)
 
 //Exportando o router para ser usado no url
 module.exports = router;
