ff7aa03ef56e8212c69c1fb7d046d908e01f21df
BeaGabriela <biiagabrielaa@gmail.com>
2025-06-02 13:56:52 -0300

    Criado o token baseado no cargo do funcionario logado, apra que apenas o gerente e o lider possam criar, alterar e deletar os sabores de pizza

--- BACKEND_NODE/src/Routes/Pizzas.routes.js
+++ BACKEND_NODE/src/Routes/Pizzas.routes.js
@@ -9,20 +9,23 @@ const pizzas = require('../Controllers/Pizzas.controller.js')
 //Verificando o nivel do cargo para editar/criar/excluir
 const verificaPermissao = require('../Middleware/Middleware_cargo.js');
 
+//Criando uma variavel que herda a funções do arquivo middleware
+const Middle = require('../Middleware/Middleware');
+
 //Definindo o corpo da URL, para puxr todas as pizzas.
 router.get("/pizzas", pizzas.MostrarPizzas)
 
 //Definindo o corpo da URL para puxar pizzas filtradas pelo id
 router.get("/pizzas/:id_pizza", pizzas.MonstrarPizzaFiltradaID)
 
-//Definindo o corpo da URL para criar uma pizza nova
-router.post("/pizzas", verificaPermissao, pizzas.CriarPizza)
+//Definindo o corpo da URL para criar uma pizza nova, de aordo com o funconario logado e com o cargo dele.
+router.post("/pizzas", Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']), pizzas.CriarPizza)
 
-//Definindo o corpo da URL para alterar dados das pizzas
-router.put("/pizzas/alterar", verificaPermissao, pizzas.AlterarPizza)
+//Definindo o corpo da URL para alterar dados das pizzas de aordo com o funconario logado e com o cargo dele.
+router.put("/pizzas/alterar", Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']), pizzas.AlterarPizza)
 
-//Definindo o corpo da URL para deletar cadastro
-router.delete("/pizzas/deletar", verificaPermissao, pizzas.DeletarPizza)
+//Definindo o corpo da URL para deletar cadastro de aordo com o funconario logado e com o cargo dele.
+router.delete("/pizzas/deletar", Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']), pizzas.DeletarPizza)
 
 //Exportando o router para ser usado no url
 module.exports = router;
