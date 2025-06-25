1b2e41ee5f375ff876233a5657fb3e2eecd478cf
BeaGabriela <biiagabrielaa@gmail.com>
2025-06-25 14:03:00 -0300

    adicionado o modelo para criar a documentação no swagger

--- BACKEND_NODE/src/Routes/Funcionarios.routes.js
+++ BACKEND_NODE/src/Routes/Funcionarios.routes.js
@@ -11,6 +11,39 @@ const verificaPermissao = require('../Middleware/Middleware_cargo.js');
 
 const Middle = require('../Middleware/Middleware');
 
+
+/** Criando docuemntação
+ * @swagger
+ * /funcionarios:
+ *   get:
+ *     summary: Lista os funcionarios
+ *     tags:
+ *       - Funcionarios
+ *     responses:
+ *       200:
+ *         description: Lista de funcionarios retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   nome:
+ *                     type: string
+ *                     example: João Silva
+ *                   cpf:
+ *                     type: string
+ *                     example: "11111111-55"
+ *                   senha:
+ *                     type: string
+ *                     example: "123"
+ *                   cargo:
+ *                     type: string
+ *                     example: "Operadora"
+ */
+
+
 //Definindo o corpo da URL, para puxr todos os funcionarois.
 router.get("/funcionarios", funcionarios.MostrarFuncionarios)
 
@@ -20,12 +53,106 @@ router.get("/funcionarios/:id_funcionario", funcionarios.MonstrarFuncionarioID)
 //Definindo o corpo da URL para que o funcionario possa logar
 router.post('/funcionario/logar',funcionarios.Logar)
 
+
+/** Criando docuemntação
+ * @swagger
+ * /funcionarios:
+ *   post:
+ *     summary: Cria novos funcionarios
+ *     tags:
+ *       - Funcionarios
+ *     responses:
+ *       201:
+ *         description: Funcionario criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   nome:
+ *                     type: string
+ *                     example: João Silva
+ *                   cpf:
+ *                     type: string
+ *                     example: "11111111-55"
+ *                   senha:
+ *                     type: string
+ *                     example: "123"
+ *                   cargo:
+ *                     type: string
+ *                     example: "Operadora"
+ */
 //Definindo o corpo da URL para criar um funcioario novo, de acordo com o funcionario logado
 router.post("/funcionario", Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']), funcionarios.CriarFuncionario)
+
+
+/** Criando docuemntação
+ * @swagger
+ * /funcionarios:
+ *   put:
+ *     summary: Altera dados do funcionario
+ *     tags:
+ *       - Funcionarios
+ *     responses:
+ *       200:
+ *         description: Dados so funcionario alterado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   nome:
+ *                     type: string
+ *                     example: João Silva
+ *                   cpf:
+ *                     type: string
+ *                     example: "11111111-55"
+ *                   senha:
+ *                     type: string
+ *                     example: "123"
+ *                   cargo:
+ *                     type: string
+ *                     example: "Operadora"
+ */
 
 //Definindo o corpo da URL para alterar dados pessoais,de acordo com o funcionario logado
 router.put("/funcionario/alterar", Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']), funcionarios.AlterarDadosFuncionario)
 
+
+/** Criando docuemntação
+ * @swagger
+ * /funcionarios:
+ *   delete:
+ *     summary: Deleta um funcionario
+ *     tags:
+ *       - Funcionarios
+ *     responses:
+ *       200:
+ *         description: Funcionario deleatdo com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   nome:
+ *                     type: string
+ *                     example: João Silva
+ *                   cpf:
+ *                     type: string
+ *                     example: "11111111-55"
+ *                   senha:
+ *                     type: string
+ *                     example: "123"
+ *                   cargo:
+ *                     type: string
+ *                     example: "Operadora"
+ */
 //Definindo o corpo da URL para deletar cadastro, de acordo com o funcionario logado
 router.delete("/funcionario/deletar",  Middle.Autenticar, verificaPermissao(['Gerente', 'Lider']),funcionarios.DeletarFuncionario)
 
