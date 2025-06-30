87189a95130c59504c9da6e13b7be457ca6538bd
BeaGabriela <biiagabrielaa@gmail.com>
2025-06-30 13:14:07 -0300

    Criado a documentação basica da rota de listar historico de status e de cadastrar

--- BACKEND_NODE/src/Routes/Historico_status.routes.js
+++ BACKEND_NODE/src/Routes/Historico_status.routes.js
@@ -10,9 +10,79 @@ const status = require('../Controllers/Historico_status.controller.js')
 //Criando uma variavel que herda a funções do arquivo middleware
 const Middle = require('../Middleware/Middleware');
 
+
+/** Criando a documentação da rota de listar os historico de status
+ *@swagger
+ * /status:
+ *   get:
+ *     summary: Lista todos os status
+ *     tags:
+ *       - Histórico de Status
+ *     responses:
+ *       200:
+ *         description: Lista de status retornado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   status_anterior:
+ *                     type: varchar
+ *                     example: 'Em preparo'
+ *                   status_novo:
+ *                     type: varchar
+ *                     example: 'Concluido'
+ *                   data_alteracao:
+ *                     type: datetime
+ *                     example: 2025-05-02 12:02:00
+ *                   pedido_id:
+ *                     type: int
+ *                     example: 1
+ * 
+ */
+
 //Definindo o corpo da URL, para puxr todas os pedidos.
 router.get("/historico", status.MostrarHistorico)
 
+/** Criando a documentação da rota de listar os historico de status
+ *@swagger
+ * /status:
+ *   post:
+ *     summary: Cadastra status
+ *     tags:
+ *       - Histórico de Status
+ *     responses:
+ *       201:
+ *         description: Status cadastrado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   status_anterior:
+ *                     type: varchar
+ *                     example: 'Em preparo'
+ *                   status_novo:
+ *                     type: varchar
+ *                     example: 'Concluido'
+ *                   data_alteracao:
+ *                     type: datetime
+ *                     example: 2025-05-02 12:02:00
+ *                   pedido_id:
+ *                     type: int
+ *                     example: 1
+ * 
+ */
 //Definindo o corpo da URL para criar um pedido novo
 router.post("/historico", status.criarStatus)
 
